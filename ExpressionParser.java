ef638fd0d79c86bc8a33f20a8d8b2f4af2e8b6ca
EmilisJ <45522661+EmilisJ@users.noreply.github.com>
2019-08-30 00:16:50 +0300

    veikia bet dalyba nelygi daugybai

--- ExpressionParser.java
+++ ExpressionParser.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 // import com.sun.swing.internal.plaf.basic.resources.basic;
 
 public class ExpressionParser {
@@ -14,7 +13,7 @@ public class ExpressionParser {
 
         for ( int i=0; i < expression.length(); ++i){
             if( !(Character.isDigit(expression.charAt(i))) && "+-*/()".indexOf(expression.charAt(i)) == -1 ){
-                IllegalArgumentException b = new IllegalArgumentException("Emilis, String contains invalid characters");
+                IllegalArgumentException b = new IllegalArgumentException("Emilis, String contains invalid characters.");
                 throw b;
             } 
         }
@@ -26,9 +25,8 @@ public class ExpressionParser {
             }
         }
 
-        
         if( ")/*+".indexOf(expression.charAt(0)) != -1 || "-(/*+".indexOf(expression.charAt(expression.length()-1)) != -1){
-            IllegalArgumentException d = new IllegalArgumentException("Emilis, String starts or ends with an invalid operator");
+            IllegalArgumentException d = new IllegalArgumentException("Emilis, String starts or ends with an invalid operator.");
             throw d;
         }
 
@@ -45,10 +43,96 @@ public class ExpressionParser {
             IllegalArgumentException e = new IllegalArgumentException("Emilis, there is an opening or closing bracket missing.");
             throw e;
         }
+        
+        ArrayList<String> cutText = new ArrayList<String>();   
+        int result = 0;
 
+        String number = "";
+        for ( int i=0; i < expression.length(); ++i){
+            if ( Character.isDigit(expression.charAt(i)) ) {
+                number += String.valueOf(expression.charAt(i));
+            } else if( !Character.isDigit(expression.charAt(i)) ){
+                if (number.length() > 0){
+                    cutText.add(number);
+                }    
+                number = "";
+                cutText.add(expression.substring(i, i+1));
+            } 
+        }
+        cutText.add(number);
+        // System.out.println(cutText);
 
+        if( cutText.size() == 1){
+            result = Integer.parseInt(cutText.get(0));
+        } 
+        
+        // int mult = cutText.indexOf("*");
+        // int div = cutText.indexOf("/");
+        // int add = cutText.indexOf("+");
+        // int sub = cutText.indexOf("-");
+        System.out.println(cutText);
+        while(cutText.size() > 1){
+            // for(int i = 0; i < cutText.size(); ++i){}
+            if(cutText.indexOf("*") != -1){
+                result = Integer.valueOf(cutText.get(cutText.indexOf("*")-1)) * Integer.valueOf(cutText.get(cutText.indexOf("*")+1));
+                // System.out.println(Integer.valueOf(cutText.get(cutText.indexOf("*")-1)));
+                // System.out.println(Integer.valueOf(cutText.get(cutText.indexOf("*")+1)));
+                // System.out.println(result);
+                cutText.set(cutText.indexOf("*")-1, String.valueOf(result));
+                cutText.remove(cutText.indexOf("*")+1);
+                cutText.remove(cutText.indexOf("*"));
+            } else if(cutText.indexOf("*") == -1 && cutText.indexOf("/") != -1){
+                result = Integer.valueOf(cutText.get(cutText.indexOf("/")-1)) / Integer.valueOf(cutText.get(cutText.indexOf("/")+1));
+                // System.out.println(result);
+                cutText.set(cutText.indexOf("/")-1, String.valueOf(result));
+                cutText.remove(cutText.indexOf("/")+1);
+                cutText.remove(cutText.indexOf("/"));
+            } else{
+                if (cutText.indexOf("+") != -1){
+                    result = Integer.valueOf(cutText.get(cutText.indexOf("+")-1)) + Integer.valueOf(cutText.get(cutText.indexOf("+")+1));
+                    cutText.set(cutText.indexOf("+")-1, String.valueOf(result));
+                    cutText.remove(cutText.indexOf("+")+1);
+                    cutText.remove(cutText.indexOf("+"));
+                }
+                if (cutText.indexOf("-") != -1){
+                    result = Integer.valueOf(cutText.get(cutText.indexOf("-")-1)) - Integer.valueOf(cutText.get(cutText.indexOf("-")+1));
+                    cutText.set(cutText.indexOf("-")-1, String.valueOf(result));
+                    cutText.remove(cutText.indexOf("-")+1);
+                    cutText.remove(cutText.indexOf("-"));
+                }
 
-        return 0;
+            }
+            // System.out.println(cutText.get(0));
+            // System.out.println(cutText.indexOf("*"));
+
+        }
+
+        //     System.out.println(cutText);
+        //     // System.out.println(cutText.indexOf("*"));
+        // for(int i = 1; i < cutText.size()-1; ++i){
+        //     int a = Integer.parseInt(cutText.get(i-1));
+        //     int b = Integer.parseInt(cutText.get(i+1));
+        //     int c = 0;
+        //     System.out.println(a);
+        //     System.out.println(b);
+        //     // System.out.println(cutText.get(1)); 
+        //     System.out.println(cutText.get(i));
+        //     if(cutText.get(i) == "*"){
+        //         System.out.println(a);
+        //         c = a * b;
+        //         // System.out.println(a);
+        //     } else if(cutText.get(i) == "/") {
+        //         c = a / b;
+        //     } else {
+        //         c = a + b;
+        //     }
+        //     cutText.remove(i-1);
+        //     cutText.remove(i-1);
+        //     cutText.set(i-1, String.valueOf(c));
+        //     System.out.println(c);
+        // }
+          
+        return result;
     }
 
     public static void main(String[] args){        
@@ -74,7 +158,7 @@ public class ExpressionParser {
             // new TestParams("2!3()/*-+", 0), // invalid characters
             // new TestParams("2-*/3/3-64", 0), // two or more operators in a row
             // new TestParams("(2(3+5)", 0),
-            new TestParams(")2+2(", 0),
+            // new TestParams(")2+2(", 0),
             // new TestParams("1", 1),
             // new TestParams("1+3", 4),
             // new TestParams("1+3", 4),
@@ -88,8 +172,8 @@ public class ExpressionParser {
             // new TestParams("3-6", -3),
             // new TestParams("60/12", 5),
             // new TestParams("0/3", 0),
-            // new TestParams("10+8*2/4", 14),
-            // new TestParams("0/3", 0),
+            // new TestParams("10*8", 40),
+            new TestParams("100+4*5-8/4*2", 320),
             // new TestParams("0/3", 0),
             // new TestParams("0/3", 0),
             // new TestParams("0/3", 0),
@@ -112,58 +196,3 @@ public class ExpressionParser {
 
     }
 }
-
-
-// StringBuilder text = new StringBuilder(100);
-
-// ArrayList<String> cutText = new ArrayList<String>();
-
-// // string not empty.
-// if(expression.length() == 0 || expression == ""){
-//     IllegalArgumentException a = new IllegalArgumentException ( "String is empty");
-//     throw a;
-// }
-
-// // check, that all chars are digits or valid operators.
-// for ( int i=0; i < expression.length(); ++i){
-//     if( Character.isDigit(expression.charAt(i)) || "+-*/()".indexOf(expression.charAt(i)) != -1 ){
-//         text.append(expression.charAt(i));
-//     } else {
-//         continue;
-//     }
-// }
-
-// // get Array list of all separate elements.
-// String number = "";
-// for ( int i=0; i < text.length(); ++i){
-
-//     if ( Character.isDigit(text.charAt(i)) ) {
-//         number += String.valueOf(text.charAt(i));
-
-//     } else if( !Character.isDigit(text.charAt(i)) ){
-//         if (number.length() > 0){
-//             cutText.add(number);
-//         }    
-//         number = "";
-//         cutText.add(text.substring(i, i+1));
-//     } 
-// }
-// cutText.add(number);
-// number = "";
-
-// // if there is only one value in arraylist- that is the result.
-// int result = 0;
-// if( cutText.size() == 1){
-//     result = Integer.parseInt(cutText.get(0));
-// } 
-
-// // while(cutText.get(0) == "+" ){
-// //     if (cutText.get(0) == "+" || cutText.get(0) == "*" || cutText.get(0) == "/"){
-// //         cutText.remove(0);
-// //     } else if(cutText.get(0) == '-') {
-// //         cutText.set(1, ("-"+ cutText.get(1)) );
-// //     }
-// //     cutText.remove(0);
-// // }
-
-
